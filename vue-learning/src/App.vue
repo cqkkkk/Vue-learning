986af6f9754b7baa737da89be595f9bb21d25fc2
CQK <1296437659@qq.com>
2018-08-28 20:49:38 +0800

    add dragmove

--- vue-learning/src/App.vue
+++ vue-learning/src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <dragmove></dragmove>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import dragmove from './components/DragMove.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    dragmove,
+  },
+  data(){
+    return{
+
+    };
   }
 }
 </script>

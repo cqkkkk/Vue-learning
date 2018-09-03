01b2bcd4e983996af4ec9317d19dfd0a43df6975
CQK <1296437659@qq.com>
2018-09-03 16:44:59 +0800

    add components change(real)

--- vue-learning/src/App.vue
+++ vue-learning/src/App.vue
@@ -2,10 +2,16 @@
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div id="selecters">
+      <input type="button" value="dragmove" v-on:click="seedragmove">
+      <input type="button" value="getnowtime" v-on:click="seegetnowtime">
+      <input type="button" value="clock" v-on:click="seeclock">
+      <input type="button" value="dragdrop" v-on:click="seedragdrop">
+    </div>
     <dragmove v-show="seendragmove"></dragmove>
     <getnowtime v-show= "seengetnowtime"></getnowtime>
-    <clock></clock>
-    <!-- <draganddrop></draganddrop> -->
+    <clock v-show="seenclock"></clock>
+    <draganddrop v-show="seendragdrop"></draganddrop>
   </div>
 </template>
 
@@ -27,9 +33,35 @@ export default {
   },
   data() {
     return {
-      seendragmove: false,
-      seengetnowtime: false
+      seendragmove: true,
+      seengetnowtime: false,
+      seenclock:false,
+      seendragdrop:false
     };
+  },
+  methods:{
+    empty:function(){
+      this.seendragmove = false;
+      this.seengetnowtime = false;
+      this.seenclock = false;
+      this.seendragdrop = false;
+    },
+    seedragmove:function(){
+      this.empty();
+      this.seendragmove = true;
+    },
+    seegetnowtime:function(){
+      this.empty();
+      this.seengetnowtime = true;
+    },
+    seeclock:function(){
+      this.empty();
+      this.seenclock = true;
+    },
+    seedragdrop:function(){
+      this.empty();
+      this.seendragdrop = true;
+    }
   }
 };
 </script>
@@ -41,6 +73,14 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#selecters{
+  position: absolute;
+    width: 15%;
+    font-size: 40px;
+    top: 10%;
+}
+#selecters input{
+  font-size: 25px;
 }
 </style>
